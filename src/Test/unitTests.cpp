d87270d125cf03cff0b184d450acbad28a964791
Sophie-2023 <sophie.siniavski@gmail.com>
2025-06-17 14:00:42 +0200

    Premier test troupe

--- src/Test/unitTests.cpp
+++ src/Test/unitTests.cpp
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
 
 #include "TourdeGuet.h"
-TEST(TestGame, FirstTest) {
-  EXPECT_EQ(1, 1);
-}
+#include "Chasseur.h"
+#include "Carte.h"
+#include "Game.h"
+
 
 TEST(Defense, TakeDamage) { 
   TourDeGuet defense(20,20);
@@ -11,3 +12,26 @@ TEST(Defense, TakeDamage) {
   EXPECT_FLOAT_EQ(defense.getPv(), 95);
 }
 
+TEST(ChasseurTest, RecevoirDegatsReduitPv) {
+
+
+  sf::RenderWindow mWindow{sf::VideoMode({1280, 720}), "Attack Tower Game"};
+  Game game(mWindow);
+  Carte carte;
+  pugi::xml_document doc;
+  if (auto result = doc.load_file("res/map.xml"); !result) {
+    std::cerr << "Could not open file visage.xml because "
+              << result.description() << std::endl;
+  }
+  pugi::xml_node nodeSource = doc.child("map");
+  carte.makeCarte(nodeSource, &game);
+
+  Chasseur chasseur(carte.getBase());
+
+  int pvInitial = chasseur.getPv();
+  int degats = 10;
+
+  chasseur.recevoirDegats(-degats);
+
+  EXPECT_EQ(chasseur.getPv(), pvInitial - degats);
+}
