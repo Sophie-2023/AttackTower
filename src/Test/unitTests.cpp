cf1910c20901c036f1db5ef76cd3d76dc03e1ba1
Helo1204 <heloise.havy@gmail.com>
2025-06-17 15:41:09 +0200

    test degat defense

--- src/Test/unitTests.cpp
+++ src/Test/unitTests.cpp
@@ -1,5 +1,13 @@
 #include <gtest/gtest.h>
 
+#include "TourdeGuet.h"
 TEST(TestGame, FirstTest) {
   EXPECT_EQ(1, 1);
 }
+
+TEST(Defense, TakeDamage) { 
+  TourDeGuet defense(20,20);
+  defense.recevoirDegats(-5);
+  EXPECT_FLOAT_EQ(defense.getPv(), 95);
+}
+
