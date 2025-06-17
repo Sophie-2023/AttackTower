10cf67fae4e4375cfdbd7a7d51e8eb2655e7826a
Helo1204 <heloise.havy@gmail.com>
2025-06-17 14:55:56 +0200

    diag uml

--- src/core/EtatRetour.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-#include "EtatRetour.h"
-
-void EtatRetour::agir(Troupe& troupe, sf::Time elapsedTime) {
-  //std::cout << "Je reviens" << std::endl;
-}
--- src/core/EtatRetour.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "State.h"
-#include "Troupe.h"
-
-class EtatRetour : public State {
- public:
-  void agir(Troupe& troupe, sf::Time elapsedTime) override;
-};
